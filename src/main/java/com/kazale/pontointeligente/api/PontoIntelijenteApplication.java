ac48a425cfff61f80ab25c67a412ef5f9d0df45c
bruno-sergio <bruno.ferrer2@gmail.com>
2020-01-25 13:09:08 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/com/kazale/pontointeligente/api/PontoIntelijenteApplication.java
@@ -0,0 +1,13 @@
+package com.kazale.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoIntelijenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoIntelijenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kazale/pontointeligente/api/PontoIntelijenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kazale.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoIntelijenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
